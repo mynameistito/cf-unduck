264dc93105243b9835f95c9b0ddde8b22c1a456b
Tito <github@mynameistito.com>
2026-05-08 19:00:16 +1200

    feat(footer): link author to personal site, add source repo link

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,5 +2,10 @@
 export const SITE = {
   name: "cf-unduck", // used in page title and OpenSearch
   domain: "search.mynameistito.com", // your deployed URL (also set in wrangler.jsonc)
-  githubUser: "mynameistito", // shown in the footer credit link
+  githubUser: "mynameistito", // GitHub user/org for the repo link
+  repo: "cf-unduck", // GitHub repo name, linked in the footer
+  author: {
+    name: "mynameistito", // shown in the footer credit
+    url: "https://mynameistito.com", // where the credit name links to
+  },
 } as const;

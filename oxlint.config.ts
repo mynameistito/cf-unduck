60d86f0ea96e8fc0b343220d17e379b2a479242c
My Name is Tito <github@mynameistito.com>
2026-08-24 14:24:55 +1200

    ultracite: add anti-slop, eslint-plugin-github, eslint-plugin-sonarjs, oxlint-plugin-react-doctor plugins

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -2,8 +2,19 @@ import { defineConfig } from "oxlint";
 import core from "ultracite/oxlint/core";
 import react from "ultracite/oxlint/react";
 import tanstack from "ultracite/oxlint/tanstack";
+import tanstackJsPlugins from "ultracite/oxlint/tanstack/js-plugins";
+import antiSlop from "ultracite/oxlint/anti-slop";
+import { jsPluginSettings, selectJsPlugins } from "ultracite/oxlint/js-plugins";
 
 export default defineConfig({
-  extends: [core, react, tanstack],
+  extends: [
+    core,
+    react,
+    tanstack,
+    tanstackJsPlugins,
+    antiSlop,
+    selectJsPlugins(["github", "sonarjs", "react-doctor"]),
+  ],
   ignorePatterns: core.ignorePatterns,
+  settings: jsPluginSettings,
 });
